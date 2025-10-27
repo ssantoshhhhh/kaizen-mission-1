4e5a2e40f24745a30ed0bfba6e0bcb8953505452
santosh <santoshkumar90101s@gmail.com>
2025-10-27 18:48:33 +0530

    installed tailwind

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ProductDashboard from "./ProductDashboard";
+import "./App.css";
+
+function App() {
+  return(
+    <>
+    <div ClassName="App">
+      <ProductDashboard/>
+    </div>
+    </>
+  )
+}
+
+export default App;
---
+++ dashboard/src/ProductDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductDashboard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProductDashboard
